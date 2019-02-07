8467aaf070ac174ba9b48e65798ab2f51e2456b6
rennyfernandez <43186486+rennyfernandez@users.noreply.github.com>
2019-02-07 07:49:40 -0500

    Update orange_seller.c

--- orange_seller.c
+++ orange_seller.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
-#include <math.h>
-
 int main(void) {
     
-    int oranges;
+    int orangesNum;
     double totalCrates,crates=16;
     
     
-    printf(" Hi.I am an orange seller. WHow many oranges do you want ?");
-    scanf("%d", &oranges);
+    printf(" Hi.I am the orange seller. How many oranges do you want ?");
+    scanf("%d", &orangesNum);
     
-    printf(" oh %d oranges?? thats a lot !\n\n", oranges);
+    printf(" oh %d oranges?? thats a lot !\n\n", orangesNum);
     printf("Anyway I sell crates of oranges. Each crate has 16 oranges\n");
     
-    totalCrates= (double)(oranges/crates);
-    printf("You need to buy %0.1lf crates to get %d oranges\n\n", ceil(totalCrates), oranges);
+    totalCrates= (double)(orangesNum/crates);
+    printf("You need to buy %0.1lf crates to get %d oranges\n\n", ceil(totalCrates), orangesNum);
     
     return 0;
-    
     
 }

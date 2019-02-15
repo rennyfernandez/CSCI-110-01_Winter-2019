fee254fde815de58060e4b9b386976045b028db2
rennyfernandez <43186486+rennyfernandez@users.noreply.github.com>
2019-02-15 15:05:31 -0500

    Create IFusher.c

---
+++ IFusher.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int noofpeople;
+    printf(" how many are in your team ?");
+    scanf("%d", &noofpeople);
+        if (noofpeople > 10) {
+        printf("please go the conference room \n\n");
+        }
+        else if (noofpeople > 2 && noofpeople < 10  ) {
+            printf("please go to the booth \n\n");
+            }
+            if (noofpeople <= 2) {
+                printf("Sorry you are not allowed here..This is a party place\n\n");
+            }
+return 0;
+}

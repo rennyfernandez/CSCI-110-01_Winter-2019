1482ba8e991cad6edd43eb2d7bf0ffabce7ab92b
rennyfernandez <43186486+rennyfernandez@users.noreply.github.com>
2019-02-15 15:08:07 -0500

    Update IFusher.c

--- IFusher.c
+++ IFusher.c
@@ -1,18 +1,17 @@
-#include <stdio.h>
+#include < stdio.h >
 
-int main(void) {
+  int main(void) {
 
     int noofpeople;
     printf(" how many are in your team ?");
-    scanf("%d", &noofpeople);
-        if (noofpeople > 10) {
-        printf("please go the conference room \n\n");
-        }
-        else if (noofpeople > 2 && noofpeople < 10  ) {
-            printf("please go to the booth \n\n");
-            }
-            if (noofpeople <= 2) {
-                printf("Sorry you are not allowed here..This is a party place\n\n");
-            }
-return 0;
-}
+    scanf("%d", & noofpeople);
+    if (noofpeople > 10) {
+      printf("please go the conference room \n\n");
+    } else if (noofpeople > 2 && noofpeople < 10) {
+      printf("please go to the booth \n\n");
+    }
+    if (noofpeople <= 2) {
+      printf("Sorry you are not allowed here..This is a party place\n\n");
+    }
+    return 0;
+  }
